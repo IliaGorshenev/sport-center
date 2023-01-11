bf38b234e4c35b9deb3399e8a8e411f85ded0ab6
Илья Горшенев <gorschenyov.ilya2014@yandex.ru>
2023-01-11 14:53:44 +0300

    Блоки

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,44 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 
+const validatePhone = function () {
+  const eventCalllback = function (e) {
+    const el = e.target;
+    const clearVal = el.dataset.phoneClear;
+    const pattern = el.dataset.phonePattern;
+    const matrixDef = '+_ (___) ___-__-__';
+    let matrix = pattern ? pattern : matrixDef;
+    let i = 0;
+    const def = matrix.replace(/\D/g, '');
+    let val = e.target.value.replace(/\D/g, '');
+    if (clearVal !== 'false' && e.type === 'blur') {
+      if (val.length < matrix.match(/([\_\d])/g).length) {
+        e.target.value = '';
+        return;
+      }
+    }
+    if (def.length >= val.length) {
+      val = def;
+    }
+    e.target.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+    });
+  };
+  const PHONE_INPUTS = document.querySelectorAll('[data-phone-pattern]');
+  for (let elem of PHONE_INPUTS) {
+    for (let ev of ['input', 'blur', 'focus']) {
+      elem.addEventListener(ev, eventCalllback);
+    }
+  }
+};
+
 const desktopBreakpoint = window.matchMedia(`(min-width: 1199px)`);
 const tabletBreakpoint = window.matchMedia(`(min-width: 980px)`);
 const mobileBreakpoint = window.matchMedia(`(min-width: 767px)`);
 
 const breakpointChecker = () => {
   if (desktopBreakpoint.matches) {
-    const swiper = new Swiper('.swiper', {
+    const swiper1 = new Swiper('.swiper--1', {
       // Optional parameters
       loop: true,
       slidesPerView: 4,
@@ -16,14 +47,14 @@ const breakpointChecker = () => {
 
       // Navigation arrows
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next--1',
+        prevEl: '.swiper-button-prev--1',
       },
     });
   }
 
   else if (tabletBreakpoint.matches) {
-    const swiper = new Swiper('.swiper', {
+    const swiper1 = new Swiper('.swiper--1', {
       // Optional parameters
       loop: true,
       slidesPerView: 3,
@@ -32,14 +63,14 @@ const breakpointChecker = () => {
 
       // Navigation arrows
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next--1',
+        prevEl: '.swiper-button-prev--1',
       },
     });
   }
 
   else if (mobileBreakpoint.matches) {
-    const swiper = new Swiper('.swiper', {
+    const swiper1 = new Swiper('.swiper--1', {
       // Optional parameters
       loop: true,
       slidesPerView: 2,
@@ -48,14 +79,14 @@ const breakpointChecker = () => {
 
       // Navigation arrows
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next--1',
+        prevEl: '.swiper-button-prev--1',
       },
     });
   }
-  
+
   else {
-    const swiper = new Swiper('.swiper', {
+    const swiper1 = new Swiper('.swiper--1', {
       // Optional parameters
       loop: true,
       slidesPerView: 1,
@@ -64,8 +95,8 @@ const breakpointChecker = () => {
 
       // Navigation arrows
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next--1',
+        prevEl: '.swiper-button-prev--1',
       },
     });
   }
@@ -75,6 +106,26 @@ desktopBreakpoint.addListener(breakpointChecker);
 tabletBreakpoint.addListener(breakpointChecker);
 mobileBreakpoint.addListener(breakpointChecker);
 breakpointChecker();
+
+const swiper2 = new Swiper('.swiper--2', {
+  // Optional parameters
+  loop: false,
+  slidesPerView: 1,
+  // freeMode: true,
+
+  mousewheel: {
+    invert: false,
+
+    releaseOnEdges: true,
+    sensitivity: 0
+},
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next--2',
+    prevEl: '.swiper-button-prev--2',
+  },
+});
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -84,6 +135,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   iosVhFix();
 
+
   // Modules
   // ---------------------------------
 
@@ -91,6 +143,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
+    validatePhone();
   });
 });
 
